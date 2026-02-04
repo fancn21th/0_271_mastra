296988e8b5a51bea347c36e3eb315696d4c75422
wuxbl <wuxbl@digitalchina.com>
2026-02-04 16:16:24 +0800

    📦 NEW: 添加 block 组件

--- src/app/playground/MessageList.tsx
+++ src/app/playground/MessageList.tsx
@@ -1,9 +1,10 @@
 "use client";
-
+import { StatusTag } from "@/components/wuhan/composed/status-tag";
 export function MessageList() {
   return (
     <div className="flex-1 overflow-y-auto px-4">
       <div>消息列表</div>
+       <StatusTag status="confirmed" />
     </div>
   );
 }
